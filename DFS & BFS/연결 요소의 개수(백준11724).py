bd82537395bb0de0399ad2d165741564b085914c
mopil1102@gmail.com <mopil1102@gmail.com>
2022-01-14 22:31:38 +0900

    .

---
+++ DFS & BFS/연결 요소의 개수(백준11724).py
@@ -0,0 +1,29 @@
+nodes, edges = map(int, input().split())
+graph = [[] for _ in range(nodes+1)]
+for i in range(edges):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+count = 0
+visited = []
+
+def dfs(graph, start):
+
+    global visited
+    stack = list()
+    stack.append(start)
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend(graph[node])
+
+
+for i in range(1,nodes):
+    if i not in visited:
+        dfs(graph, i)
+        count += 1
+
+print(count)
