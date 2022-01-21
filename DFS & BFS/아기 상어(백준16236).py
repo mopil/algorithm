e2a1c51b944d9e427c96b3efc0677ee0d66a93bf
mopil1102@gmail.com <mopil1102@gmail.com>
2022-01-22 01:25:54 +0900

    아기 상어(예제4 빼고 다 통과)

--- DFS & BFS/아기 상어(백준16236).py
+++ DFS & BFS/아기 상어(백준16236).py
@@ -1,18 +1,5 @@
-from collections import deque
 import time
-
-# class Fish:
-#     fish_serial = 0
-# 
-#     def __init__(self, x, y, size):
-#         self.x = x
-#         self.y = y
-#         self.size = size
-#         Fish.fish_serial += 1
-#         self.no = Fish.fish_serial
-# 
-#     def __str__(self):
-#         return f"{self.no}번 물고기 좌표:{self.x, self.y}, 사이즈:{self.size}"
+from collections import deque
 
 
 class Shark:
@@ -20,19 +7,14 @@ class Shark:
         self.x = x
         self.y = y
         self.size = 2  # 상어 초기 사이즈는 2
-        self.fishes = 0
-
-    def __str__(self):
-        return f"좌표:{self.x, self.y}, 사이즈:{self.size}"
+        self.fishes = 0  # 먹은 물고기들
 
 
-def find_eatable_fish(x, y):
+def find_eatable_fish(shark_x, shark_y):
     q = deque()
-    q.append((x, y))
+    q.append((shark_x, shark_y))
     field_copy = [row[:] for row in field]
     eatable_fishes = []
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
 
     while q:
         x, y = q.popleft()
@@ -50,60 +32,36 @@ def find_eatable_fish(x, y):
                 if (nx, ny) not in eatable_fishes:
                     eatable_fishes.append((nx, ny))
 
-            if (field_copy[nx][ny] == 0 or fish_size == shark.size) and field_copy[nx][ny] != 8:
-                field_copy[nx][ny] = 8
+            if (field_copy[nx][ny] == 0 or fish_size == shark.size) and field_copy[nx][ny] != 9:
+                field_copy[nx][ny] = 9
                 q.append((nx, ny))
 
-    eatable_fishes.sort()
-    return eatable_fishes[0]
+    # 더이상 먹을 물고기가 없으면 -1, -1을 리턴
+    if not eatable_fishes:
+        return -1, -1
+    # 그게 아니면 가장 가까운 물고기 좌표를 찾아서 리턴
+    else:
+        #eatable_fishes.sort()
+        result = {}
+        for fish_coord in eatable_fishes:
+            result[(fish_coord[0], fish_coord[1])] = bfs(shark_x, shark_y, fish_coord[0], fish_coord[1], "find")
 
-    # while q:
-    #     x, y = q.popleft()
-    #     print_field(field_copy)
-    #     print_field(visited)
-    #     print("--------------------------------")
-    #     for i in range(4):
-    #         nx = x + dx[i]
-    #         ny = y + dy[i]
-    #
-    #         if nx < 0 or nx >= n or ny < 0 or ny >= n:
-    #             continue
-    #
-    #         fish_size = field_copy[nx][ny]
-    #
-    #         if fish_size < shark.size and field_copy[nx][ny] != 0:
-    #             # 기존에 상어 위치를 물고기를 먹은 위치로 바꾼다
-    #             field[init_x][init_y] = 0
-    #             field[nx][ny] = 9
-    #
-    #             shark.fishes.append(1)  # 먹은 물고기 추가
-    #
-    #             visited[nx][ny] = visited[x][y] + 1
-    #
-    #             return nx, ny, visited[nx][ny]  # 물고기를 먹어치운 위치를 리턴
-    #
-    #         if (field_copy[nx][ny] == 0 or fish_size == shark.size) and field_copy[nx][ny] != 8:
-    #             field_copy[nx][ny] = 8
-    #             visited[nx][ny] = visited[x][y] + 1
-    #             q.append((nx, ny))
-    #
-    # return init_x, init_y, 0  # 먹을 물고기가 없으면 기존 상어 위치를 리턴
+        target = min(list(result.values()))
+        for item in result.items():  # result 에는 좌표가 키로, 상어가 해당 좌표로 이동하는 이동횟수가 벨류로 들어가 있음
+            if item[1] == target:
+                return item[0]
 
 
-def bfs(shark_x, shark_y, fish_x, fish_y):
+def bfs(shark_x, shark_y, fish_x, fish_y, mode):
     q = deque()
     init_x, init_y = shark_x, shark_y
     q.append((shark_x, shark_y))
     field_copy = [row[:] for row in field]
     visited = [([0] * n) for _ in range(n)]
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
 
     while q:
         x, y = q.popleft()
-        # print_field(field_copy)
-        # print_field(visited)
-        # print("--------------------------------")
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -113,7 +71,8 @@ def bfs(shark_x, shark_y, fish_x, fish_y):
 
             fish_size = field[nx][ny]
 
-            if nx == fish_x and ny == fish_y:
+            # 상어가 실제로 물고기를 먹는 bfs
+            if nx == fish_x and ny == fish_y and mode == "eat":
                 # 기존에 상어 위치를 물고기를 먹은 위치로 바꾼다
                 field[init_x][init_y] = 0
                 field[nx][ny] = 9
@@ -122,14 +81,21 @@ def bfs(shark_x, shark_y, fish_x, fish_y):
 
                 visited[nx][ny] = visited[x][y] + 1
 
-                return nx, ny, visited[nx][ny]  # 물고기를 먹어치운 위치를 리턴
+                shark_coord.append((nx, ny))  # 물고기를 먹은 후 해당 위치를 상어 위치로 저장
 
-            if (field_copy[nx][ny] == 0 or fish_size == shark.size) and field_copy[nx][ny] != 8:
-                field_copy[nx][ny] = 8
+                return visited[nx][ny]
+
+            # 상어가 주변 물고기를 찾을 때 호출되는 bfs (차이점은 field의 변경 여부)
+            if nx == fish_x and ny == fish_y and mode == "find":
+                visited[nx][ny] = visited[x][y] + 1
+                return visited[nx][ny]
+
+            if (field_copy[nx][ny] == 0 or fish_size == shark.size) and field_copy[nx][ny] != 9:
+                field_copy[nx][ny] = 9
                 visited[nx][ny] = visited[x][y] + 1
                 q.append((nx, ny))
                 
-    return init_x, init_y, 0  # 먹을 물고기가 없으면 기존 상어 위치를 리턴
+    return 0
 
 
 def check_shark_size():
@@ -137,19 +103,17 @@ def check_shark_size():
     if shark.fishes == shark.size:
         shark.fishes = 0
         shark.size += 1
-        return True
-    return False
 
 
-# def check_end():
-#     # 끝나는 조건
-#     # 1. 남아 있는 물고기가 하나도 없을 경우
-#     # 2. 남아 있는 물고기가 상어 사이즈보다 모두 크거나 같을 경우
-#     for i in range(n):
-#         for k in range(1, 7):
-#             if field[i].count(k) != 0:
-#                 return False
-#     return True
+def check_end():
+    # 끝나는 조건
+    # 1. 남아 있는 물고기가 하나도 없을 경우 -> 물고기가 남아 있을 경우 False
+    # 2. 남아 있는 물고기가 상어 사이즈보다 모두 크거나 같을 경우 -> 사이즈가 작은 물고기가 존재하면 False
+    for i in range(n):
+        for j in range(n):
+            if field[i][j] <= shark.size:
+                return False
+    return True
 
 
 def print_field(field_copy):
@@ -157,7 +121,6 @@ def print_field(field_copy):
         for j in range(n):
             print(field_copy[i][j], end=" ")
         print()
-    print()
 
 
 n = int(input())
@@ -165,34 +128,40 @@ field = []
 for _ in range(n):
     field.append(list(map(int, input().split())))
 
-# 자신보다 크면 못지나감. 작으면 먹기 가능. 같으면 먹지는 못하고 지나가기만 가능
+# 상어 세팅
 for i in range(n):
     for j in range(n):
         if field[i][j] == 9:
             shark = Shark(i, j)
-            a, b = i, j
+            now_shark_x, now_shark_y = i, j
+shark_coord = deque()
+shark_coord.append((now_shark_x, now_shark_y))
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 total_move = 0
+
+# 메인 로직
+# 1. 현재 상어 위치에서 현재 크기로 먹을 수 있는 모든 물고기 위치를 탐색한다
+# 2. 물고기 위치를 정렬하여 가장 작은값을 돌려준다
+# 3. 해당 위치로 가서 물고기를 먹는다
+# 4. 끝나는 조건을 만족할 때까지 위를 반복한다
 while True:
-    # 메인 로직
-    # 1. 현재 상어 위치에서 현재 크기로 먹을 수 있는 모든 물고기 위치를 탐색한다
-    # 2. 물고기 위치를 정렬하여 가장 작은값을 돌려준다
-    # 3. 해당 위치로 가서 물고기를 먹는다
-    # 4. 끝나는 조건을 만족할 때까지 위를 반복한다
-    try:
-        fish_x, fish_y = find_eatable_fish(a, b)
-    except:
+    now_shark_x, now_shark_y = shark_coord.popleft()
+
+    e_fish_x, e_fish_y = find_eatable_fish(now_shark_x, now_shark_y)
+
+    if e_fish_x == -1 and e_fish_y == -1 or check_end() or shark.size >= 9:
         break
 
-    a, b, move = bfs(a, b, fish_x, fish_y)
-    print(f"움직인 횟수:{move}, 샤크크기:{shark.size}")
-
-    total_move += move
-
-    # 상어 사이즈 체크
+    move = bfs(now_shark_x, now_shark_y, e_fish_x, e_fish_y, "eat")
+    print_field(field)
     check_shark_size()
-
+    total_move += move
+    print(f"움직인 횟수:{move}, 총 이동수:{total_move}, 상어크기:{shark.size}, 먹은 물고기:{shark.fishes}\n")
 print(total_move)
+
+
 
 
 
