4ccfbf69fc0770441addbd0cc669943447e680b4
mopil1102@gmail.com <mopil1102@gmail.com>
2022-01-23 15:58:03 +0900

    아기 상어

--- DFS & BFS/아기 상어(백준16236).py
+++ DFS & BFS/아기 상어(백준16236).py
@@ -1,43 +1,57 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
+# def print_field(field_copy):
+#     for i in range(n):
+#         for j in range(n):
+#             print(field_copy[i][j], end=" ")
+#         print()
 
 
+# 현재 위치에서 먹을 수 있는 물고기를 탐색한다
 def bfs():
-    global total_move, eat_fishes, shark_size, shark_x, shark_y
+    global shark_size, shark_x, shark_y
     q = deque()
-    q.append((shark_x, shark_y))
-    visited = [([0] * n) for _ in range(n)]
-    fish = []
+    q.append((shark_x, shark_y, 0))
+    visited = [([False] * n) for _ in range(n)]
+    visited[shark_x][shark_y] = True
+    fish_list = []
+    min_dist = int(1e9)
 
-    # 현재 위치에서 먹을 수 있는 물고기를 탐색한다
     while q:
-        x, y = q.popleft()
+        x, y, dist = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if field[nx][ny] <= shark_size:
+                    visited[nx][ny] = True
 
-            if field[nx][ny] > shark_size or visited[nx][ny]:
-                continue
+                    if 0 < field[nx][ny] < shark_size:
+                        min_dist = dist
+                        fish_list.append((dist+1, nx, ny))
 
-            if 0 < field[nx][ny] < shark_size and not visited[nx][ny]:
-                visited[nx][ny] = visited[x][y] + 1
-                fish.append((visited[nx][ny], nx, ny))  # 물고기의 위치와 그 물고기를 먹으려면 이동해야 하는 횟수를 저장
+                    if dist+1 <= min_dist:
+                        q.append((nx, ny, dist+1))
+    return fish_list
 
-            # 비어있거나 사이즈도 작고, 한번도 방문 안한 경우
-            q.append((nx, ny))
-            visited[nx][ny] = visited[x][y] + 1
-    #print_field(field)
-
+    # print("필드:")
+    # print_field(field)
+    # print("\n방문:")
+    # print_field(visited)
     # 먹을 물고기가 없으면 False
-    if len(fish) <= 0:
+
+
+def eat(fish_list):
+    global total_move, shark_x, shark_y, eat_fishes, shark_size
+    if len(fish_list) <= 0:
         return False
     # 물고리를 먹는다 (물고기 위치랑 상어 위치를 바꾸고 사이즈를 더한다)
     else:
-        fish.sort()
-        move, fish_x, fish_y = fish[0][0], fish[0][1], fish[0][2]
+        move, fish_x, fish_y = min(fish_list)
         total_move += move
 
         field[shark_x][shark_y] = 0
@@ -45,19 +59,12 @@ def bfs():
         shark_x, shark_y = fish_x, fish_y
 
         eat_fishes += 1
-        #print(f"움직인 횟수:{move}, 총 이동수:{total_move}, 상어크기:{shark_size}, 먹은 물고기:{eat_fishes}\n")
+        # print(f"움직인 횟수:{move}, 총 이동수:{total_move}, 상어크기:{shark_size}, 먹은 물고기:{eat_fishes}\n")
         if eat_fishes == shark_size:
             eat_fishes = 0
             shark_size += 1
 
         return True
-
-
-# def print_field(field_copy):
-#     for i in range(n):
-#         for j in range(n):
-#             print(field_copy[i][j], end=" ")
-#         print()
 
 
 n = int(input())
@@ -77,10 +84,11 @@ total_move = 0
 shark_size = 2
 eat_fishes = 0
 
-while bfs() and shark_size < 9:
-    pass
+while True:
+    fish_list = bfs()
+    if not eat(fish_list):
+        break
 print(total_move)
-
 
 
 
