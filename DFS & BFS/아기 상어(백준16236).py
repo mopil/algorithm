14f81a429f5603d6252a35382e19c98740b48812
mopil1102@gmail.com <mopil1102@gmail.com>
2022-01-23 16:09:29 +0900

    아기 상어 (시간 초과 원인 분석)

--- DFS & BFS/아기 상어(백준16236).py
+++ DFS & BFS/아기 상어(백준16236).py
@@ -2,12 +2,6 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-# def print_field(field_copy):
-#     for i in range(n):
-#         for j in range(n):
-#             print(field_copy[i][j], end=" ")
-#         print()
-
 
 # 현재 위치에서 먹을 수 있는 물고기를 탐색한다
 def bfs():
@@ -38,11 +32,38 @@ def bfs():
                         q.append((nx, ny, dist+1))
     return fish_list
 
-    # print("필드:")
-    # print_field(field)
-    # print("\n방문:")
-    # print_field(visited)
-    # 먹을 물고기가 없으면 False
+
+'''
+숨바꼭질 처럼 visited에 방문 횟수를 더하면서 진행하면 시간초과가 뜬다 (아마 2차원배열이기 때문에 그런거지 싶다)
+그래서 이럴 경우에는 visited를 False로 초기화하고 방문하면 True 한다음에 2차원 배열에 거리 값을 저장하지 않고 따로 다른곳(ex. 큐)에 관리하여야 한다(위 bfs 처럼)
+아래 함수는 위 bfs와 똑같은 로직이지만 거리값을 2차원 배열에 넣었는데 시간초과가 발생함을 확인했다
+'''
+def bfs_2차원더하기방식():
+    global shark_size, shark_x, shark_y
+    q = deque()
+    q.append((shark_x, shark_y, 0))
+    visited = [([0] * n) for _ in range(n)]
+    fish_list = []
+    min_dist = int(1e9)
+
+    while q:
+        x, y, dist = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if field[nx][ny] <= shark_size:
+                    visited[nx][ny] = visited[x][y] + 1
+
+                    if 0 < field[nx][ny] < shark_size:
+                        min_dist = visited[nx][ny]
+                        fish_list.append((visited[nx][ny], nx, ny))
+
+                    if visited[nx][ny] <= min_dist:
+                        q.append((nx, ny, visited[nx][ny]))
+    return fish_list
 
 
 def eat(fish_list):
@@ -59,7 +80,6 @@ def eat(fish_list):
         shark_x, shark_y = fish_x, fish_y
 
         eat_fishes += 1
-        # print(f"움직인 횟수:{move}, 총 이동수:{total_move}, 상어크기:{shark_size}, 먹은 물고기:{eat_fishes}\n")
         if eat_fishes == shark_size:
             eat_fishes = 0
             shark_size += 1
