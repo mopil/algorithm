bc079f558d27e98bf04abf5b5096ba6c4f3e79eb
mopil1102 <mopil1102@gmail.com>
2022-01-26 02:41:08 +0900

    파일 정리 (shorter)

--- Sorting/파일 정리(백준20291).py
+++ Sorting/파일 정리(백준20291).py
@@ -1,20 +1,10 @@
 n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(input())
-
-확장자 = []
-for string in arr:
-    dot_index = string.find('.')
-    확장자.append(string[dot_index+1:])
-
 result = {}
-for i in range(len(확장자)):
-    if 확장자[i] not in result.keys():
-        result[확장자[i]] = 1
+for _ in range(n):
+    temp = input().split('.')[1]
+    if temp not in result.keys():
+        result[temp] = 1
     else:
-        result[확장자[i]] += 1
-
-keys = sorted(list(result.keys()))
-for i in keys:
-    print(i, result[i])
+        result[temp] += 1
+for k, v in sorted(result.items()):
+    print(k, v)
