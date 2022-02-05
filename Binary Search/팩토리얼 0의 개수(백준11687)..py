16d13ee319e4be9dd13e7bfe08bd48901756d285
mopil1102@gmail.com <mopil1102@gmail.com>
2022-02-05 23:35:56 +0900

    팩토리얼 0의 개수 (refactoring)

--- Binary Search/팩토리얼 0의 개수(백준11687)..py
+++ Binary Search/팩토리얼 0의 개수(백준11687)..py
@@ -2,38 +2,30 @@
 https://www.acmicpc.net/problem/11687
 팩토리얼 0의 개수 (백준 11687) 골드 5
 '''
-
 import math
 
-
 def find_right_zeros(n):
-    digit = str(n)
-    zero_count = 0
-    for i in range(len(digit), 0, -1):
-        if digit[i-1] == '0':
-            zero_count += 1
-        else:
-            break
-    return zero_count
+    zeros = 0
+    while n >= 5:
+        zeros += n // 5
+        n //= 5
+    return zeros
 
-
-fac_list = [math.factorial(i) for i in range(1, 10**2)]
 
 m = int(input())
-result = 0
-start, end = 0, len(fac_list) - 1
-while start <= end:
-    mid = (start + end) // 2
+left, right, result = 1, m * 5, 0
 
-    zero_count = find_right_zeros(fac_list[mid])
+while left <= right:
+    mid = (left + right) // 2
 
+    # 메인 로직
+    zero_count = find_right_zeros(mid)
+
+    # 조건 분기
     if zero_count < m:
-        start = mid + 1
+        left = mid + 1
     else:
-        end = mid - 1
+        right = mid - 1
         result = mid
 
-if find_right_zeros(fac_list[result]) != m:
-    print(-1)
-else:
-    print(result+1)
+print(result if find_right_zeros(result) == m else -1)
