d6b36ddbf4cf1529de4444ed239891dfff0f21eb
mopil1102 <mopil1102@gmail.com>
2022-02-08 22:08:03 +0900

    카펫

---
+++ Brute Force/카펫(프로그래머스lv.2).py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    for i in range(3, total+1):
+        q = total // i  # 세로
+        p = total // q  # 가로
+        total_brown = (p + q) * 2 - 4
+        if total_brown == rown and p >= q and p * q == total:
+            return [p, q]
