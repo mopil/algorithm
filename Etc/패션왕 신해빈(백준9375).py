8cd9699a20ab80626bdb12947e13d52aab990512
mopil1102 <mopil1102@gmail.com>
2022-02-11 01:34:09 +0900

    패션왕 신해빈 (조합, 실패)

--- Etc/패션왕 신해빈(백준9375).py
+++ Etc/패션왕 신해빈(백준9375).py
@@ -1,48 +1,34 @@
 from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+
 test_case = int(input())
 for t in range(test_case):
-    temp_items = list()
-    item_num = int(input())
-    # 일단 튜플로 리스트에 저장
-    for i in range(item_num):
-        item_name, item_category = map(str, input().split())
-        temp_items.append((item_category, item_name))
-
-    # 이제 반복문을 돌면서 딕셔너리에 저장
-    items = {item[0]: [] for item in temp_items}
-    for item in temp_items:
-        items[item[0]].append(item[1])
+    items = {}
+    for i in range(int(input())):
+        not_use, item = map(str, input().split())
+        if item not in items.keys(): items[item] = 1
+        else: items[item] += 1
     #print(items)
 
-    # 경우의 수 계산 = 각 카테고리별 아이템 수 + (각 아이템 수 전부 곱)
-    item_key = list(items.keys())
-    #for k in range(1, len(item_key)+1):
-        #print(list(combinations(item_key, k)))
-        #for j in combinations(item_key, k):  # j = item_category
+    # 경우의 수 계산
+    result = 0
+    for way in range(1, len(items.keys())+1):
+        for cw in combinations(items.keys(), way):
+            total_way = 1
+            for c in cw: total_way *= items[c]
+            #print(cw, total_way)
+            result += total_way
+    print(result)
 
 
-
-
-
-
-    # each_category_item_num = 0
-    # each_item_product = 1
-    # if len(list(items.keys())) == 1:
-    #     each_item_product = 0
-    # for k in items.keys():
-    #     each_category_item_num += len(items[k])
-    #     each_item_product *= len(items[k])
-    #
-    # print(each_category_item_num + each_item_product)
-#
 # 1
-# 3
+# 7
 # mask face
 # sunglasses face
 # makeup face
-
-# 1
-# 3
-# hat headgear
-# sunglasses eyewear
-# turban headgear
+# hi tu
+# bye tu
+# a alb
+# b alb
