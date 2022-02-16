8a1ea48f2ea1600136cae8d76689dd8d9c923369
mopil1102 <mopil1102@gmail.com>
2022-02-16 16:46:46 +0900

    간선 이어가기2

---
+++ Shortest Path/Dijkstra/간선 이어가기2(백준14284).py
@@ -0,0 +1,27 @@
+import heapq
+
+n, m = map(int, input().split())
+INF = int(1e9)
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+for _ in range(m):
+    s, e, c = map(int, input().split())
+    graph[s].append((e, c))
+    graph[e].append((s, c))
+
+start, end = map(int, input().split())
+
+q = []
+heapq.heappush(q, (0, start))
+distance[start] = 0
+while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+        continue
+    for i in graph[now]:
+        cost = dist + i[1]
+        if cost < distance[i[0]]:
+            distance[i[0]] = cost
+            heapq.heappush(q, (cost, i[0]))
+
+print(distance[end])
