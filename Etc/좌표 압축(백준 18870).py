4ea2210220a6e5c99d798d42e8d36c1c7c4fe148
mopil1102 <mopil1102@gmail.com>
2022-03-10 11:10:04 +0900

    좌표 입력

--- Etc/좌표 압축(백준 18870).py
+++ Etc/좌표 압축(백준 18870).py
@@ -1,25 +1,10 @@
+import sys
+
+input = sys.stdin.readline
 n = int(input())
 coord = list(map(int, input().split()))
-sorted_coord = sorted(coord)
-i = 0
-tmp = []
-index = 0
-while i < len(sorted_coord):
-    before = sorted_coord[i - 1]
-    now = sorted_coord[i]
-    if i >= 1 and before == now:
-        pair = tmp[-1]
-    else:
-        pair = (sorted_coord[i], index)
-        index += 1
-    i += 1
-    tmp.append(pair)
+sorted_coord = sorted(list(set(coord)))
+dic = {sorted_coord[i] : i for i in range(len(sorted_coord))}
+for i in coord:
+    print(dic[i], end=" ")
 
-result = []
-for n in coord:
-    for v, i in tmp:
-        if n == v:
-            result.append(i)
-            break
-
-print(" ".join(str(i) for i in result))
