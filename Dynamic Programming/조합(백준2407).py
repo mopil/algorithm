1a4841c1b9db3bdc12d48b7a74883ec4ffed18e7
mopil1102@gmail.com <mopil1102@gmail.com>
2022-03-25 21:45:22 +0900

    조합

--- Dynamic Programming/조합(백준2407).py
+++ Dynamic Programming/조합(백준2407).py
@@ -1,3 +1,28 @@
+import sys
 import math
+sys.setrecursionlimit(10000)
 n, m = map(int, input().split())
-print(math.factorial(n)//(math.factorial(n-m)*math.factorial(m)))
+# print(math.factorial(n)//(math.factorial(n-m)*math.factorial(m)))
+
+
+# def comb(n, r):
+#     return math.factorial(n)//(math.factorial(n-r)*math.factorial(r))
+
+def comb(n, r):
+    if n == r:
+        return 1
+    elif r == 1:
+        return n
+    if dp[n][r] != 0:
+        return dp[n][r]
+    dp[n][r] = comb(n-1, r) + comb(n-1, r-1)
+    return dp[n][r]
+
+
+dp = [[0] * (n+1) for _ in range(n+1)]
+
+print(comb(n, m))
+
+
+
+
