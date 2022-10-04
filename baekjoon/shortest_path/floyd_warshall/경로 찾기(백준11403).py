4a9121387452222fd3a1c7d05c0af861af5e8633
Seong Heung Bae <mopil1102@gmail.com>
2022-10-04 23:44:10 +0900

    경로 찾기

---
+++ baekjoon/shortest_path/floyd_warshall/경로 찾기(백준11403).py
@@ -0,0 +1,14 @@
+
+'''
+플로이드-와샬 버전
+'''
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+for line in graph:
+    print(*line)
